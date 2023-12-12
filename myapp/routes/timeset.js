e7fb56c15ba63bcabf19a0f7d837535b9a169ddd
hoon <csh3004@naver.com>
2023-12-12 18:11:36 +0900

    fix/timeset 오류 수정

--- myapp/routes/timeset.js
+++ myapp/routes/timeset.js
@@ -4,14 +4,12 @@ var router = express.Router();
 const timeset = [];
 
 router.post('/send', (req, res) =>{
-    const {day, time} = req.body;
-    
-    if (!day || !time) {
+    const newTimeset = req.body;
+    if (!timeset) {
         res.status(400).send({ message: '요일과 시간을 선택해주세요' });
         return;
       }
-
-    timeset.push(req.body);
+    timeset.push(newTimeset);
     console.log(timeset)
     res.send("일정 저장 완료 완료")
   })
