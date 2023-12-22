536b3b6cc5aa0d9712ac5d19a70529b4f6525caa
hoon <csh3004@naver.com>
2023-12-23 02:09:02 +0900

    feat: user table

---
+++ myapp/models/users.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model{
+    static init(sequelize){
+        return super.init({
+            name : {
+                type : Sequelize.STRING(20),
+                allowNull : false,
+                unique : true,
+            },
+            userId : {
+                type : Sequelize.STRING(20),
+                allowNull : false,
+                unique : true
+            },
+            userPw : {
+                type : Sequelize.STRING(20),
+                allowNull : false,
+                unique : false
+            },
+            age : {
+                type : Sequelize.INTEGER.UNSIGNED,
+                allowNull : false,
+            },
+            created_at : {
+                type : Sequelize.DATE,
+                allowNull : false,
+                defaultValue : Sequelize.NOW
+            }
+        },{
+            sequelize,
+            timestamps : false,
+            underscored : true,
+            modelName : "User",
+            tableName : "users",
+            paranoid : false,
+            collate : "utf8_general_ci",
+            charset : "utf8",
+        })
+    }
+};
