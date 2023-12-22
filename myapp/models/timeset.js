952672cfa8aed6fffcd13720638dad62ae3c75dd
hoon <csh3004@naver.com>
2023-12-23 03:25:16 +0900

    feat/models 구조 수정 및 관계 설정
    user - timeset
    1.     -      M

--- myapp/models/timeset.js
+++ myapp/models/timeset.js
@@ -1,39 +1,49 @@
+// timeset.js
 const Sequelize = require('sequelize');
 
-module.exports = class Timeset extends Sequelize.Model{
-    static init(sequelize){
-        return super.init({
-            userId : {
-                type : Sequelize.STRING(20),
-                allowNull : false,
-                unique : true,
-                primaryKey : true
-            },
-            day : {
-                type : Sequelize.STRING(20),
-                allowNull : false,
-                unique : false
-            },
-            time : {
-                type : Sequelize.STRING(20),
-                allowNull : false,
-                unique : false
-            },
-            created_at : {
-                type : Sequelize.DATE,
-                allowNull : false,
-                defaultValue : Sequelize.NOW
-            }
+module.exports = class Timeset extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        userId: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
+          primaryKey: true,
         },
-        {
-            sequelize,
-            timestamps : false,
-            underscored : true,
-            modelName : "Timeset",
-            tableName : "timeset",
-            paranoid : false,
-            collate : "utf8_general_ci",
-            charset : "utf8",
-        })
-    }
+        day: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        time: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        number: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'Timeset',
+        tableName: 'timeset',
+        paranoid: false,
+        collate: 'utf8_general_ci',
+        charset: 'utf8',
+      }
+    );
+  }
+
+  static associate(models) {
+    // Timeset 모델과 User 모델 간의 다대일(Many-to-One) 관계 설정
+    this.belongsTo(models.User, { foreignKey: 'userId', targetKey: 'userId' });
+  }
 };
--- myapp/models/users.js
+++ myapp/models/users.js
@@ -1,42 +1,50 @@
+// user.js
 const Sequelize = require('sequelize');
 
-module.exports = class User extends Sequelize.Model{
-    static init(sequelize){
-        return super.init({
-            name : {
-                type : Sequelize.STRING(20),
-                allowNull : false,
-                unique : true,
-            },
-            userId : {
-                type : Sequelize.STRING(20),
-                allowNull : false,
-                unique : true
-            },
-            userPw : {
-                type : Sequelize.STRING(20),
-                allowNull : false,
-                unique : false
-            },
-            age : {
-                type : Sequelize.INTEGER.UNSIGNED,
-                allowNull : false,
-            },
-            created_at : {
-                type : Sequelize.DATE,
-                allowNull : false,
-                defaultValue : Sequelize.NOW
-            }
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
         },
-        {
-            sequelize,
-            timestamps : false,
-            underscored : true,
-            modelName : "User",
-            tableName : "users",
-            paranoid : false,
-            collate : "utf8_general_ci",
-            charset : "utf8",
-        })
-    }
+        userId: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
+        },
+        userPw: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: false,
+        },
+        age: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.NOW,
+        },
+      },
+      {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'User',
+        tableName: 'users',
+        paranoid: false,
+        collate: 'utf8_general_ci',
+        charset: 'utf8',
+      }
+    );
+  }
+
+  static associate(models) {
+    // User 모델과 Timeset 모델 간의 일대다(One-to-Many) 관계 설정
+    this.hasMany(models.Timeset, { foreignKey: 'userId', sourceKey: 'userId' });
+  }
 };
